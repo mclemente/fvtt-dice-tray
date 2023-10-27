5952da145e5204282d8f463fcac2e93d66309a5a
Matheus Clemente <matheus.xb@gmail.com>
2023-10-27 17:41:34 -0300

    Add Star Wars FFG support

--- src/module/maps/_module.js
+++ src/module/maps/_module.js
@@ -4,6 +4,7 @@ export { default as dcc } from "./dcc.js";
 export { default as dnd5e } from "./dnd5e.js";
 export { default as ModularFate, default as fateCoreOfficial, default as fatex } from "./fate.js";
 export { default as pf2e } from "./pf2e.js";
+export { default as starwarsffg } from "./starwarsffg.js";
 export { default as swade } from "./swade.js";
 // export { default as tresdetv } from "./tresdetv.js";
 
---
+++ src/module/maps/starwarsffg.js
@@ -0,0 +1,45 @@
+import GenericDiceMap from "./templates/template.js";
+
+export default class starwarsffgDiceMap extends GenericDiceMap {
+	get dice() {
+		return [
+			{
+				dp: {
+					tooltip: game.i18n.localize("SWFFG.DiceProficiency"),
+					img: "systems/starwarsffg/images/dice/starwars/yellow.png",
+					color: "#fef135"
+				},
+				da: {
+					tooltip: game.i18n.localize("SWFFG.DiceAbility"),
+					img: "systems/starwarsffg/images/dice/starwars/green.png",
+					color: "#46ac4f"
+				},
+				dc: {
+					tooltip: game.i18n.localize("SWFFG.DiceChallenge"),
+					img: "systems/starwarsffg/images/dice/starwars/red.png",
+					color: "#751317"
+				},
+				di: {
+					tooltip: game.i18n.localize("SWFFG.DiceDifficulty"),
+					img: "systems/starwarsffg/images/dice/starwars/purple.png",
+					color: "#52287e"
+				},
+				db: {
+					tooltip: game.i18n.localize("SWFFG.DiceBoost"),
+					img: "systems/starwarsffg/images/dice/starwars/blue.png",
+					color: "#76c3db"
+				},
+				ds: {
+					tooltip: game.i18n.localize("SWFFG.DiceSetback"),
+					img: "systems/starwarsffg/images/dice/starwars/black.png",
+					color: "#141414"
+				},
+				df: {
+					tooltip: game.i18n.localize("SWFFG.DiceForce"),
+					img: "systems/starwarsffg/images/dice/starwars/whiteHex.png",
+					color: "#ffffff"
+				}
+			}
+		];
+	}
+}
