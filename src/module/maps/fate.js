01d87bbee5320b2245fb68574b6bbf17850ddda0
Matheus Clemente <matheus.xb@gmail.com>
2023-11-02 23:18:31 -0300

    Update fate.js

--- src/module/maps/fate.js
+++ src/module/maps/fate.js
@@ -4,7 +4,6 @@ export default class FateDiceMap extends GenericDiceMap {
 	get dice() {
 		return [
 			{
-				d6: { img: "icons/dice/d6black.svg" },
 				"4df": { label: game.i18n.localize("DICE_TRAY.FateDice")},
 			}
 		];
