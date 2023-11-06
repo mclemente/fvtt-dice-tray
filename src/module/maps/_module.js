c8ce7f60db45ed5941b033b0ec8861555b0222d2
Matheus Clemente <matheus.xb@gmail.com>
2023-11-06 02:19:21 -0300

    Update _module.js

--- src/module/maps/_module.js
+++ src/module/maps/_module.js
@@ -1,11 +1,10 @@
 export { default as Template } from "./templates/template.js";
 
 export { default as dcc } from "./dcc.js";
+export { default as demonlord } from "./demonlord.js";
 export { default as dnd5e } from "./dnd5e.js";
 export { default as ModularFate, default as fateCoreOfficial, default as fatex } from "./fate.js";
 export { default as pf2e } from "./pf2e.js";
 export { default as starwarsffg } from "./starwarsffg.js";
 export { default as swade } from "./swade.js";
-export { default as demonlord } from "./demonlord.js";
-// export { default as tresdetv } from "./tresdetv.js";
 
