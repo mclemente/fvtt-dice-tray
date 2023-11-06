bede2f581edcff1cafdb08a9dee9c4081f8047f2
Matheus Clemente <matheus.xb@gmail.com>
2023-11-06 02:19:05 -0300

    Update _keys.js

--- src/module/maps/_keys.js
+++ src/module/maps/_keys.js
@@ -1,3 +1,7 @@
+/**
+ * Javascript imports don't support dashes so the workaround
+ * for systems with dashes in their names is to create this map.
+ */
 export const KEYS = {
 	"fate-core-official": "fateCoreOfficial"
 };
