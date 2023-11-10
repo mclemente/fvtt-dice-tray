2cfb721e2b7dba4f85410a49127078b4418f3be1
Matheus Clemente <matheus.xb@gmail.com>
2023-11-10 15:21:58 -0300

    Update _module.js

--- src/module/calculator-buttons/_module.js
+++ src/module/calculator-buttons/_module.js
@@ -1,6 +1,6 @@
 export { default as archmage } from "./archmage.js";
 export { default as dcc } from "./dcc.js";
+export { default as demonlord } from "./demonlord.js";
 export { default as dnd5e } from "./dnd5e.js";
 export { default as pf2e } from "./pf2e.js";
-export { default as demonlord } from "./demonlord.js";
 
