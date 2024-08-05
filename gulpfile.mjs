366a2fb1814794ca201367740a3d8f1712563d66
Matheus Clemente <matheus.xb@gmail.com>
2024-08-05 16:52:05 -0300

    Convert to SCSS

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,6 +5,8 @@
 
 import fs from "fs-extra";
 import gulp from "gulp";
+import prefix from "gulp-autoprefixer";
+import sass from "gulp-dart-sass";
 import sourcemaps from "gulp-sourcemaps";
 import path from "node:path";
 import buffer from "vinyl-buffer";
@@ -24,7 +26,7 @@ const packageId = "dice-calculator";
 const sourceDirectory = "./src";
 const distDirectory = "./dist";
 const stylesDirectory = `${sourceDirectory}/styles`;
-const stylesExtension = "css";
+const stylesExtension = "scss";
 const sourceFileExtension = "js";
 const staticFiles = ["assets", "fonts", "lang", "packs", "templates", "module.json"];
 
@@ -53,7 +55,14 @@ function buildCode() {
  * Build style sheets
  */
 function buildStyles() {
-	return gulp.src(`${stylesDirectory}/${packageId}.${stylesExtension}`).pipe(gulp.dest(`${distDirectory}/styles`));
+	return gulp.src([`${stylesDirectory}/${packageId}.${stylesExtension}`], { base: stylesDirectory })
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+		.pipe(prefix({
+			cascade: false
+		}))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(`${distDirectory}/styles`));
 }
 
 /**
